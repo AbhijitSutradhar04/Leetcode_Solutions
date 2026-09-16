f06567e40ff610f9e1caee0d95556056e9e04a5f
Abhijit Sutradhar <sutradhardipali58@gmail.com>
2026-09-16 23:13:09 +0530

    Time: 0 ms (100%), Space: 19.3 MB (29.01%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        ans = []
+        s = "123456789"
+
+        # length of numbers
+        for length in range(2, 10):
+            for i in range(10 - length):
+                num = int(s[i:i+length])
+
+                if low <= num <= high:
+                    ans.append(num)
+
+        return sorted(ans)

6f6a2463e604e5b4069e22b5c62a85d805560713
Abhijit Sutradhar <sutradhardipali58@gmail.com>
2026-09-22 21:53:26 +0530

    Time: 335 ms (82.19%), Space: 34.4 MB (31.51%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.py
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.py
@@ -1,23 +1,26 @@
 class Solution:
-    def resultArray(self, nums: List[int], k: int) -> List[int]:
+    def resultArray(self, nums, k):
         ans = [0] * k
         
         # dp[r] = number of subarrays ending at previous index
-        # whose product % k == r
+        # with product % k == r
         dp = [0] * k
         
         for num in nums:
             new_dp = [0] * k
             
-            # Start a new subarray with only num
-            new_dp[num % k] += 1
+            rem = num % k
+            
+            # Start new subarray containing only num
+            new_dp[rem] += 1
             
             # Extend previous subarrays
             for r in range(k):
                 if dp[r]:
-                    new_dp[(r * num) % k] += dp[r]
+                    new_rem = (r * rem) % k
+                    new_dp[new_rem] += dp[r]
             
-            # Add all subarrays ending here to the answer
+            # Add all subarrays ending here to final answer
             for r in range(k):
                 ans[r] += new_dp[r]
             

68d9b77c01c798bf820ac2d01948f9552bffd2b2
Abhijit Sutradhar <sutradhardipali58@gmail.com>
2026-09-25 21:37:54 +0530

    Time: 14 ms (80.99%), Space: 19.6 MB (35.46%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        mag_count = Counter(magazine)
+        
+        for ch in ransomNote:
+            if mag_count[ch] == 0:
+                return False
+            
+            mag_count[ch] -= 1
+        
+        return True
